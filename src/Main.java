558c13be62d05c70e360370b0bb94b4124ac5d69
RAMU <ramu@RAMUs-MacBook-Air.local>
2023-02-08 23:22:32 +0545

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        String [] x={"ram","hari"};
+        int [] y={1,2,3};
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(x[0]);
+        System.out.println(y[0]);
+        System.out.println("ppp"+x.length);
+        ArrayList<String> a=new ArrayList<String>();
+        a.add("ram");
+        System.out.println(a);
+    }
+}
